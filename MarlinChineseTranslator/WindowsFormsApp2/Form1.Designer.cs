50e3e9b9311af21a5cc64e903c70e7c946cf45c6
igame2000 <igame2000@hotmail.com>
2019-10-16 11:22:51 -0700

    Add files via upload

---
+++ MarlinChineseTranslator/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.tbx_Source = new System.Windows.Forms.TextBox();
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.Btn_Restore = new System.Windows.Forms.Button();
+      this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+      this.Btn_Gen = new System.Windows.Forms.Button();
+      this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+      this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.Btn_LoadCode = new System.Windows.Forms.Button();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // tbx_Source
+      // 
+      this.tbx_Source.Location = new System.Drawing.Point(12, 31);
+      this.tbx_Source.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.tbx_Source.Multiline = true;
+      this.tbx_Source.Name = "tbx_Source";
+      this.tbx_Source.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.tbx_Source.Size = new System.Drawing.Size(880, 36);
+      this.tbx_Source.TabIndex = 0;
+      this.tbx_Source.Text = "完成取消应用加抖长直径配刷终反向上升换初化首限位错冗余高低校败失先中参心准备请绪卡已插入拔出主菜单自动开始关闭步进电机回原点设置偏移定预热所有床降温打源挤抽轴调平" +
+    "速度喷嘴风扇流控制最小大当前运耗材对比载保存恢复厂更新信息界面整暂停印继续止由储无休眠等待操作";
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Location = new System.Drawing.Point(12, 86);
+      this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(508, 259);
+      this.richTextBox1.TabIndex = 4;
+      this.richTextBox1.Text = "";
+      // 
+      // richTextBox2
+      // 
+      this.richTextBox2.Location = new System.Drawing.Point(534, 86);
+      this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.richTextBox2.Name = "richTextBox2";
+      this.richTextBox2.Size = new System.Drawing.Size(463, 259);
+      this.richTextBox2.TabIndex = 5;
+      this.richTextBox2.Text = "";
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "language_cn.h";
+      this.openFileDialog1.Filter = "language_cn.h|language_cn.h";
+      // 
+      // Btn_Restore
+      // 
+      this.Btn_Restore.Location = new System.Drawing.Point(1014, 110);
+      this.Btn_Restore.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.Btn_Restore.Name = "Btn_Restore";
+      this.Btn_Restore.Size = new System.Drawing.Size(86, 20);
+      this.Btn_Restore.TabIndex = 7;
+      this.Btn_Restore.Text = "Restore";
+      this.Btn_Restore.UseVisualStyleBackColor = true;
+      this.Btn_Restore.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // richTextBox3
+      // 
+      this.richTextBox3.Location = new System.Drawing.Point(9, 364);
+      this.richTextBox3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.richTextBox3.Name = "richTextBox3";
+      this.richTextBox3.Size = new System.Drawing.Size(1091, 371);
+      this.richTextBox3.TabIndex = 8;
+      this.richTextBox3.Text = "";
+      // 
+      // Btn_Gen
+      // 
+      this.Btn_Gen.Location = new System.Drawing.Point(1014, 136);
+      this.Btn_Gen.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.Btn_Gen.Name = "Btn_Gen";
+      this.Btn_Gen.Size = new System.Drawing.Size(86, 20);
+      this.Btn_Gen.TabIndex = 9;
+      this.Btn_Gen.Text = "Generate with new table";
+      this.Btn_Gen.UseVisualStyleBackColor = true;
+      this.Btn_Gen.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // numericUpDown2
+      // 
+      this.numericUpDown2.Location = new System.Drawing.Point(226, 7);
+      this.numericUpDown2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+      this.numericUpDown2.Minimum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+      this.numericUpDown2.Name = "numericUpDown2";
+      this.numericUpDown2.Size = new System.Drawing.Size(86, 20);
+      this.numericUpDown2.TabIndex = 10;
+      this.numericUpDown2.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+      // 
+      // richTextBox4
+      // 
+      this.richTextBox4.Location = new System.Drawing.Point(9, 754);
+      this.richTextBox4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.richTextBox4.Name = "richTextBox4";
+      this.richTextBox4.Size = new System.Drawing.Size(1091, 265);
+      this.richTextBox4.TabIndex = 11;
+      this.richTextBox4.Text = "";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(10, 9);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(99, 13);
+      this.label1.TabIndex = 12;
+      this.label1.Text = "Chinese Characters";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(10, 69);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(78, 13);
+      this.label2.TabIndex = 13;
+      this.label2.Text = "language_cn.h";
+      // 
+      // Btn_LoadCode
+      // 
+      this.Btn_LoadCode.Location = new System.Drawing.Point(1014, 84);
+      this.Btn_LoadCode.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.Btn_LoadCode.Name = "Btn_LoadCode";
+      this.Btn_LoadCode.Size = new System.Drawing.Size(86, 20);
+      this.Btn_LoadCode.TabIndex = 6;
+      this.Btn_LoadCode.Text = "Load code";
+      this.Btn_LoadCode.UseVisualStyleBackColor = true;
+      this.Btn_LoadCode.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(531, 70);
+      this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(127, 13);
+      this.label3.TabIndex = 14;
+      this.label3.Text = "Restorted language_cn.h";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(9, 348);
+      this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(131, 13);
+      this.label4.TabIndex = 15;
+      this.label4.Text = "Translated language_cn.h";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(6, 738);
+      this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(25, 13);
+      this.label5.TabIndex = 16;
+      this.label5.Text = "Log";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(151, 9);
+      this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(60, 13);
+      this.label6.TabIndex = 17;
+      this.label6.Text = "Start Code:";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1111, 1031);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.richTextBox4);
+      this.Controls.Add(this.numericUpDown2);
+      this.Controls.Add(this.Btn_Gen);
+      this.Controls.Add(this.richTextBox3);
+      this.Controls.Add(this.Btn_Restore);
+      this.Controls.Add(this.Btn_LoadCode);
+      this.Controls.Add(this.richTextBox2);
+      this.Controls.Add(this.richTextBox1);
+      this.Controls.Add(this.tbx_Source);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+      this.Name = "Form1";
+      this.Text = "Marlin Chinese Language Code Translator";
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_Source;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Btn_Restore;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button Btn_Gen;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn_LoadCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MarlinChineseTranslator/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // CAUTION:
+        // This Chinese Simplification Alphabed table is aligned with 
+        // the Marlin font. If you don't know how to modify the Marlin
+        // font, you should NOT touch this app and its code.
+        
+        // 位限率弃放化换终未中户刷到径直长率用应消取成完成击从抖起零归加
+        //const string Alphabet = "不败失先知参维准备就绪卡已插入拔出主菜单自动开始关闭步进电机回原点设置偏移定预热所有床降温打源挤抽轴调平速度喷嘴风扇流控制最小大当前运耗材对比载保存恢复厂更新信息界面整暂停印继续止由储无休眠等待操作";
+        //const string Alphabet = "完成取消应用加抖长直径配刷终反向上升换初化首限位错冗余高低校败失先中参心准备请绪卡已插入拔出主菜单自动开始关闭步进电机回原点设置偏移定预热所有床降温打源挤抽轴调平速度喷嘴风扇流控制最小大当前运耗材对比载保存恢复厂更新信息界面整暂停印继续止由储无休眠等待操作";
+        const string Alphabet = "完成就空应用加抖长直径驶刷微反按探针换初化首限位错交替高低校败失先中越心准备请绪卡已插入拔出主菜单自动开始关闭步进电机回原点设置偏移定预热所有床降温打源挤抽轴调平速度喷嘴风扇流控制最小大当前运耗材对比载保存恢复厂更新信息界面整暂停印继续止悬储无休眠等待操作";
+        const int OriginalStartCode = 151;
+        const int StartCode = 128;
+        
+        /// <summary>
+        /// Load language head file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Clear();
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        /// <summary>
+        /// Restore original characters from \x code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+
+            Regex rgx = new Regex(@"\\x[0-9a-fA-F]+");
+
+            foreach (string line in richTextBox1.Lines)
+            {
+                string str = line;
+                Match match = rgx.Match(line);
+                
+                while(match.Success)
+                {
+                    string res = str.Substring(match.Index, match.Length);
+                    string hex = res.Substring(2, 2);
+
+                    int val = Int32.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+
+                    string cnStr = Alphabet.Substring(val - (int)numericUpDown2.Value, 1);
+
+                    str = str.Replace(res, cnStr);
+
+                    match = rgx.Match(str);
+                }
+
+                richTextBox2.AppendText(str + "\n");        
+            }
+        }
+
+        /// <summary>
+        /// Generate localized head file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+
+            Dictionary<char, int> CharUsing= new Dictionary<char, int>();
+            Dictionary<char, int> CharCode = new Dictionary<char, int>();
+            Dictionary<char, int> dicUndefinedChars = new Dictionary<char, int>();
+
+            int code = (int)numericUpDown2.Value;
+
+            foreach(char ch in Alphabet)
+            {
+                CharUsing.Add(ch, 0);
+                CharCode.Add(ch, code++);
+            }
+
+            Regex rgx = new Regex("\".*\"", RegexOptions.ECMAScript);
+
+            foreach(string line in richTextBox2.Lines)
+            {
+                string str = line;
+                Match match = rgx.Match(str);
+
+                if (match.Success)
+                {
+                    string subStr = str.Substring(match.Index, match.Length);
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach(char ch in subStr)
+                    {
+                        //int pos = Alphabet.IndexOf(ch);
+                        
+                        if (CharCode.ContainsKey(ch))
+                        {
+                            sb.AppendFormat("\\x{0:x02}", CharCode[ch]);
+                            CharUsing[ch]++;
+                        } else
+                        {
+                            if (ch > 128)
+                            {
+                                if (dicUndefinedChars.ContainsKey(ch))
+                                {
+                                    dicUndefinedChars[ch]++;
+                                } else
+                                {
+                                    dicUndefinedChars[ch] = 1;
+                                }
+                            }
+
+                            sb.Append(ch);
+                        }
+                    } // foreach
+
+                    // Replace old string with hex code
+                    str = str.Replace(subStr, sb.ToString()); 
+                } // if
+
+                richTextBox3.AppendText(str + "\n");
+            } // for each
+
+            //richTextBox4.AppendText(string.Format("\nlength:{0}", richTextBox4.Text.Length));
+            richTextBox4.AppendText(string.Format("Undefined characters({0}):{1}", dicUndefinedChars.Count, new string(dicUndefinedChars.Keys.ToArray())));
+
+            StringBuilder sbUnusedChars = new StringBuilder();
+            {
+                StringBuilder sbCharUsage = new StringBuilder();
+
+                foreach (char key in CharUsing.Keys)
+                {
+                    sbCharUsage.AppendFormat("{0}({1})", key, CharUsing[key]);
+                    if (CharUsing[key] == 0)
+                    {
+                        sbUnusedChars.Append(key);
+                    }
+                }
+
+                richTextBox4.AppendText("\nUsed slots:" + sbCharUsage.ToString());
+                richTextBox4.AppendText(string.Format("\nUnused slots({1}): {0}", sbUnusedChars.ToString(), sbUnusedChars.Length));
+            }
+
+            if (sbUnusedChars.Length < dicUndefinedChars.Count) {
+                richTextBox4.AppendText(string.Format("\nUnfortunately, all slots have been used. need {0} more", sbUnusedChars.Length - dicUndefinedChars.Count));
+            } else {
+                richTextBox4.AppendText(string.Format("\nLucky, there are {0} slots left.", sbUnusedChars.Length - dicUndefinedChars.Count));
+            }
+        }
+    }
+}
